e84b06f464e595a61d1cb1f03186589089f72275
William Stewart <william@synthasite.com>
2015-09-04 10:16:10 +0200

    Clean up models
    Dropped the data property function and parts of conditionals
    that will never be evaluated differently

--- teamsupport/models.py
+++ teamsupport/models.py
@@ -6,10 +6,10 @@ from teamsupport.errors import MissingArgumentError
 class Ticket(object):
     def __init__(self, client, ticket_id=None, data=None):
         self.client = client
-        self._data = data
+        self.data = data
         if ticket_id:
-            self._data = self.client.get_ticket(ticket_id)
-        elif not (ticket_id or data):
+            self.data = self.client.get_ticket(ticket_id)
+        elif not self.data:
             raise MissingArgumentError(
                 "__init__() needs either a 'ticket_id' or 'data' argument "
                 "(neither given)")
@@ -19,10 +19,6 @@ class Ticket(object):
         if self.data.find(name) is not None:
             return self.data.find(name).text
         raise AttributeError(name)
-
-    @property
-    def data(self):
-        return self._data
 
     @cached_property
     def actions(self):
@@ -37,10 +33,10 @@ class Ticket(object):
 class Action(object):
     def __init__(self, client, ticket_id=None, action_id=None, data=None):
         self.client = client
-        self._data = data
+        self.data = data
         if action_id and ticket_id:
-            self._data = self.client.get_ticket_action(ticket_id, action_id)
-        elif not (ticket_id and action_id) and not data:
+            self.data = self.client.get_ticket_action(ticket_id, action_id)
+        elif not self.data:
             raise MissingArgumentError(
                 "__init__() needs either both a 'ticket_id' and 'action_id' "
                 "or a 'data' argument (neither given)")
@@ -51,7 +47,3 @@ class Action(object):
         if self.data.find(name) is not None:
             return self.data.find(name).text
         raise AttributeError(name)
-
-    @property
-    def data(self):
-        return self._data
