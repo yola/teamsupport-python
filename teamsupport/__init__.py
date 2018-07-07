8122bb5b7870e1f3b306f1b605818dbc7e79d8cf
Eugene Koval <eugene@yola.com>
2018-07-07 21:28:52 +0300

    Intendation

--- teamsupport/__init__.py
+++ teamsupport/__init__.py
@@ -12,8 +12,9 @@ __version__ = '0.4.1'
 __all__ = (Action, TeamSupportService, Ticket, config)
 
 
-def init(org_id, auth_key, default_ticket_type='Support',
-            default_ticket_status='New'):
+def init(
+        org_id, auth_key, default_ticket_type='Support',
+        default_ticket_status='New'):
     config.ORG_ID = org_id
     config.AUTH_KEY = auth_key
     config.DEFAULT_TICKET_TYPE = default_ticket_type
